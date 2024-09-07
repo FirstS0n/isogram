83fc79f951f2b521cf25c1beeccd7a8610a2526b
VSK <148152089+FirstS0n@users.noreply.github.com>
2024-09-07 13:51:29 +0300

    Update isogram.cpp
    лабораторная работа Курлаев

--- isogram/isogram.cpp
+++ isogram/isogram.cpp
@@ -1,14 +1,24 @@
+//я очень прошу прощения если что-то не так сделал, раньше гитхабом не пользовался
 #include "isogram.hpp"
 #include <set>
 #include <locale>
+using namespace std;
 
-// Для фукнций std::isalpha и std::toupper используйте эту локаль
-std::locale locale{""};
+// Для функций std::isalpha и std::toupper используйте эту локаль
+locale locale{""};
 
 auto is_isogram(std::string const& word) -> bool {
-    // Функция is_isogram получает на вход строку word
-    // Возвращает true, если строка является изограммой,
-    // и false, если не явялется
-    // Код пишите здесь
-    return word.empty();
+    if(world.empty()){return true;}
+    // Преобразуем все символы в верхний регистр
+    string hight_letter;
+    for (char c : word) {
+        hight_letter += toupper(c, locale);
+    }
+    for (size_t i=0; i < hight_letter.lenght() - 1;  i++){
+        for (size_t j=i + 1; j < hight_letter.lenght(); j++){
+            if (hight_letter[i] == hight_letter[j]){return false;}
+        }
+    }
+// была перенесена строка с пустым словом наверх для логичности кода
+    return true;
 }
